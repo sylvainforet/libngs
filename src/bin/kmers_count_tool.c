d72522955ea0da0de167c5d51d21cced6e420081
sylvainforet <sylvainforet@gmail.com>
2010-12-01 02:08:44 +1000

    Finished kmers_count_tool utility

--- src/bin/kmers_count_tool.c
+++ src/bin/kmers_count_tool.c
@@ -20,47 +20,84 @@
  *
  */
 
-int main(int argc, char **argv)
-{
-  return 0;
-}
-
-#if 0
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-#include "ngs_binseq.h"
+#include "ngs_kmerhash.h"
 
 typedef struct _CallbackData CallbackData;
 
 struct _CallbackData
 {
-  char              *input_path;
-  GIOChannel        *input_channel;
-  char              *cmd;
+  KmerHashTable     *hash_table;
+
+  char             **in_txt;
+  char             **in_bin;
+  char              *out;
 
   unsigned int       k;
-  unsigned int       k_bytes;
 
   int                verbose;
+  int                binary;
 };
 
 static void parse_args    (CallbackData      *data,
                            int               *argc,
                            char            ***argv);
 
-static void kmer_bin_load (CallbackData      *data);
+static void cleanup       (CallbackData      *data);
 
 int
 main (int    argc,
       char **argv)
 {
   CallbackData  data;
+  char        **tmp;
+  GError       *error = NULL;
 
   parse_args (&data, &argc, &argv);
-  kmer_bin_load (&data);
+
+  if (data.in_txt)
+    for (tmp = data.in_txt; *tmp != NULL; tmp++)
+      {
+        if (data.verbose)
+          g_printerr ("Loading kmer counts from text file: %s\n", *tmp);
+        kmer_hash_table_load (data.hash_table, *tmp, 0, &error);
+        if (error)
+          {
+            g_printerr ("[ERROR] Problem while loading %s: %s\n",
+                        *tmp, error->message);
+            exit (1);
+          }
+      }
+  if (data.in_bin)
+    for (tmp = data.in_bin; *tmp != NULL; tmp++)
+      {
+        if (data.verbose)
+          g_printerr ("Loading kmer counts from binary file: %s\n", *tmp);
+        kmer_hash_table_load (data.hash_table, *tmp, 1, &error);
+        if (error)
+          {
+            g_printerr ("[ERROR] Problem while loading %s: %s\n",
+                        *tmp, error->message);
+            exit (1);
+          }
+      }
+  if (data.verbose)
+    g_printerr ("Writing results to %s\n", data.out);
+  kmer_hash_table_print (data.hash_table,
+                         data.out,
+                         0,
+                         &error);
+  if (error)
+    {
+      g_printerr ("[ERROR] Problem while writing %s: %s\n",
+                  data.out, error->message);
+      exit (1);
+    }
+
+  cleanup (&data);
 
   return 0;
 }
@@ -72,8 +109,12 @@ parse_args (CallbackData      *data,
 {
   GOptionEntry entries[] =
     {
-      {"kmer",     'k', 0, G_OPTION_ARG_INT,      &data->k,           "K-mer size", NULL},
-      {"verbose",  'v', 0, G_OPTION_ARG_NONE,     &data->verbose,     "Verbose output", NULL},
+      {"kmer",     'k', 0, G_OPTION_ARG_INT,            &data->k,        "K-mer size",                   NULL},
+      {"verbose",  'v', 0, G_OPTION_ARG_NONE,           &data->verbose,  "Verbose output",               NULL},
+      {"binary",   'u', 0, G_OPTION_ARG_NONE,           &data->binary,   "Output in binary format",      NULL},
+      {"out",      'o', 0, G_OPTION_ARG_FILENAME,       &data->out,      "Output file",                  NULL},
+      {"intxt",    'a', 0, G_OPTION_ARG_FILENAME_ARRAY, &data->in_txt,   "Input text kmer count file",   NULL},
+      {"inbin",    'b', 0, G_OPTION_ARG_FILENAME_ARRAY, &data->in_bin,   "Input binary kmer count file", NULL},
       {NULL}
     };
   GError         *error = NULL;
@@ -81,8 +122,12 @@ parse_args (CallbackData      *data,
 
   data->k        = 0;
   data->verbose  = 0;
+  data->binary   = 0;
+  data->out      = NULL;
+  data->in_txt   = NULL;
+  data->in_bin   = NULL;
 
-  context = g_option_context_new ("CMD FILE - Utility to manipulate kmers count files");
+  context = g_option_context_new ("- Utility to manipulate kmers count files");
   g_option_context_add_main_entries (context, entries, NULL);
   if (!g_option_context_parse (context, argc, argv, &error))
     {
@@ -91,120 +136,34 @@ parse_args (CallbackData      *data,
     }
   g_option_context_free (context);
 
-  if (*argc < 2)
-    {
-      g_printerr ("[ERROR] No command provided\n");
-      exit (1);
-    }
-  data->cmd = (*argv)[1];
-  if (*argc < 3)
-    {
-      g_printerr ("[ERROR] No input file provided\n");
-      exit (1);
-    }
-  data->input_path = (*argv)[2];
-  if (strcmp (data->cmd, "print") == 0)
-    {
-      if (data->verbose)
-        g_printerr ("Calling the `print' command\n");
-    }
-  else
-    {
-      g_printerr ("[ERROR] Unknown command: %s\n", data->cmd);
-      exit (1);
-    }
-
   if (data->k < 1)
     {
       g_printerr ("[ERROR] The kmer size must be larger than 0\n");
       exit (1);
     }
-  data->k_bytes = (data->k + NUCS_PER_BYTE - 1) / NUCS_PER_BYTE;
+  if (!data->out)
+    data->out = g_strdup ("-");
+  if ((data->in_txt == NULL || *data->in_txt == NULL) &&
+      (data->in_bin == NULL || *data->in_bin == NULL))
+    {
+      g_printerr ("No input file provided\n");
+      exit (1);
+    }
+  data->hash_table = kmer_hash_table_new (data->k);
 }
 
 static void
-kmer_bin_load (CallbackData *data)
+cleanup (CallbackData *data)
 {
-  GError        *error     = NULL;
-  int            use_stdin = 1;
-  char          *tmp_str;
-  unsigned char *buffer;
-  gsize          buffer_size;
-
-  /* Open */
-  if (!data->input_path || !*data->input_path || (data->input_path[0] == '-' && data->input_path[1] == '\0'))
-    data->input_channel = g_io_channel_unix_new (STDOUT_FILENO);
-  else
-    {
-      use_stdin           = 0;
-      data->input_channel = g_io_channel_new_file (data->input_path, "r", &error);
-      if (error)
-        {
-          g_printerr ("[ERROR] failed to open input file `%s': %s\n",
-                      data->input_path,
-                      error->message);
-          exit (1);
-        }
-    }
-  g_io_channel_set_encoding (data->input_channel, NULL, NULL);
-  /* TODO set channel buffering? */
-
-  buffer_size      = data->k_bytes + sizeof (unsigned long int);
-  buffer           = g_alloca (buffer_size);
-  tmp_str          = g_alloca (data->k + 1);
-  tmp_str[data->k] = '\0';
-
-  while (1)
-    {
-      unsigned long int val_be;
-      unsigned long int val;
-      gsize             nb_read;
-      GIOStatus         status;
-
-      status = g_io_channel_read_chars (data->input_channel,
-                                        (char*)buffer,
-                                        buffer_size,
-                                        &nb_read,
-                                        &error);
-      if (error)
-        {
-          g_printerr ("[ERROR] Reading from input file `%s' failed: %s\n",
-                      data->input_path,
-                      error->message);
-          exit (1);
-        }
-      if (status == G_IO_STATUS_EOF || status != G_IO_STATUS_NORMAL)
-        break;
-      if (nb_read != buffer_size)
-        {
-          g_printerr ("[ERROR] Expected %ld bytes, read only %ld\n",
-                      buffer_size,
-                      nb_read);
-          exit (1);
-        }
-      bin_to_char_prealloc (tmp_str, buffer, data->k);
-      val_be = *(unsigned long int*)(buffer + data->k_bytes);
-      val    = GULONG_FROM_BE (val_be);
-      g_print ("%s %ld\n", tmp_str, val);
-
-    }
-
-  /* Close */
-  if (!use_stdin)
-    {
-      g_io_channel_shutdown (data->input_channel, TRUE, &error);
-      if (error)
-        {
-          g_printerr ("[ERROR] Closing input file `%s' failed: %s\n",
-                      data->input_path,
-                      error->message);
-          g_error_free (error);
-        }
-    }
-  g_io_channel_unref (data->input_channel);
+  if (data->in_txt)
+    g_strfreev (data->in_txt);
+  if (data->in_bin)
+    g_strfreev (data->in_bin);
+  if (data->out)
+    g_free (data->out);
+  if (data->hash_table)
+    kmer_hash_table_destroy (data->hash_table);
 }
-
-#endif
 
 /* vim:ft=c:expandtab:sw=4:ts=4:sts=4:cinoptions={.5s^-2n-2(0:
  */
